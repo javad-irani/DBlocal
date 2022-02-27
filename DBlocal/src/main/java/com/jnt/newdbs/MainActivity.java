f9e9ae2910ef5479bb5570c38efdf576508d69b2
Java <jvd.irani@gmail.com>
2022-02-27 20:30:47 +0000

    Description

--- DBlocal/src/main/java/com/jnt/newdbs/MainActivity.java
+++ DBlocal/src/main/java/com/jnt/newdbs/MainActivity.java
@@ -18,7 +18,8 @@ import com.jnt.newdbs.helper.Fonts;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String LOG = "jnth main";
+
+    private String LOG = "jnth main and master";
     private String string1 = "0123456789";
     private String string2 = "1abcd2efgh3ijkm4nlop5rst";
     private String string4 = "bdhgciufjekam"; // 14
